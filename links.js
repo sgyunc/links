bd637e9f4a3a43b91562613092362e394ac340a9
上官云琛 <110713522+sgyunc@users.noreply.github.com>
2026-08-28 02:12:12 +0800

    更新 links.js

--- links.js
+++ links.js
@@ -1,27 +1,247 @@
-// links.js 友链数据，独立文件
-const linksData = [
-    {
-        name: "示例博客A",
-        url: "https://example-a.com",
-        desc: "一个技术博客站点",
-        avatar: "https://picsum.photos/id/1/120/120",
-        joinYear: 2026
-    },
-    {
-        name: "示例博客B",
-        url: "https://example-b.com",
-        desc: "个人随笔记录",
-        avatar: "https://picsum.photos/id/2/120/120",
-        joinYear: 2026
-    },
-    {
-        name: "FRDLINK",
-        url: "https://blog.frdlink.link",
-        desc: "记录生活事",
-        avatar: "https://picsum.photos/id/3/120/120",
-        joinYear: 2025
-    }
+window.linksData = [
+  {
+    "name": "杜老师说",
+    "url": "https://dusays.com",
+    "desc": "杜老师说",
+    "avatar": "https://favicone.vercel.app/icon?url=https://dusays.com",
+    "joinYear": 2026
+  },
+  {
+    "name": "Heo",
+    "url": "https://blog.zhheo.com/",
+    "desc": "设计师",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blog.zhheo.com/",
+    "joinYear": 2026
+  },
+  {
+    "name": "2222",
+    "url": "https://www.baidu.com",
+    "desc": "",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.baidu.com",
+    "joinYear": 2026
+  },
+  {
+    "name": "好站网",
+    "url": "https://haozhan.wang/",
+    "desc": "发现好站，展示好站",
+    "avatar": "https://favicone.vercel.app/icon?url=https://haozhan.wang/",
+    "joinYear": 2026
+  },
+  {
+    "name": "蛋蛋之家",
+    "url": "https://wuqishi.com",
+    "desc": "一枚蛋蛋的自留地",
+    "avatar": "https://favicone.vercel.app/icon?url=https://wuqishi.com",
+    "joinYear": 2026
+  },
+  {
+    "name": "秋記Autumn",
+    "url": "https://www.zhyok.cn/",
+    "desc": "80后个人博客、网络日杂、生活记录。",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.zhyok.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "小王子",
+    "url": "https://www.wanghao.me/",
+    "desc": "聆听小王子，守望成长，静待花开。",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.wanghao.me/",
+    "joinYear": 2026
+  },
+  {
+    "name": "姓王者",
+    "url": "https://xingwangzhe.fun/",
+    "desc": "姓王者的博客",
+    "avatar": "https://favicone.vercel.app/icon?url=https://xingwangzhe.fun/",
+    "joinYear": 2026
+  },
+  {
+    "name": "FRDLINK留言板",
+    "url": "https://gb.frdlink.link",
+    "desc": "说你想说的话，做你想做的事。",
+    "avatar": "https://favicone.vercel.app/icon?url=https://gb.frdlink.link",
+    "joinYear": 2026
+  },
+  {
+    "name": "益友网站",
+    "url": "https://blog.yiyou.bj.cn",
+    "desc": "记生活点滴，留岁月温柔。",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blog.yiyou.bj.cn",
+    "joinYear": 2026
+  },
+  {
+    "name": "旺东自留地",
+    "url": "https://wang618.cn/",
+    "desc": "爱生活、爱摸鱼",
+    "avatar": "https://favicone.vercel.app/icon?url=https://wang618.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "FRDLINK博客",
+    "url": "https://blog.frdlink.link/",
+    "desc": "一个简单的个人博客",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blog.frdlink.link/",
+    "joinYear": 2026
+  },
+  {
+    "name": "浮生事",
+    "url": "http://etom.top",
+    "desc": "无根之言，只当笑谈。",
+    "avatar": "https://favicone.vercel.app/icon?url=http://etom.top",
+    "joinYear": 2026
+  },
+  {
+    "name": "皮皮社",
+    "url": "https://www.pipishe.com",
+    "desc": "皮一下～很开心！",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.pipishe.com",
+    "joinYear": 2026
+  },
+  {
+    "name": "大佬论坛",
+    "url": "https://www.dalao.net",
+    "desc": "大佬论坛",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.dalao.net",
+    "joinYear": 2026
+  },
+  {
+    "name": "阮一峰",
+    "url": "https://www.ruanyifeng.com/blog/",
+    "desc": "阮一峰的网络日志",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.ruanyifeng.com/blog/",
+    "joinYear": 2026
+  },
+  {
+    "name": "星辰角落",
+    "url": "https://ihello.cc",
+    "desc": "记录生活点滴",
+    "avatar": "https://favicone.vercel.app/icon?url=https://ihello.cc",
+    "joinYear": 2026
+  },
+  {
+    "name": "Fiee's BLog",
+    "url": "https://www.aifiee.cn/",
+    "desc": "长风破浪会有时，直挂云帆济沧海",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.aifiee.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "黑锋",
+    "url": "http://waphfw.com/a.asp?o=ZZZ",
+    "desc": "黑锋破解联盟-始于2011年,14年专注wap网址导航 Waphfw.Com",
+    "avatar": "https://favicone.vercel.app/icon?url=http://waphfw.com/a.asp?o=ZZZ",
+    "joinYear": 2026
+  },
+  {
+    "name": "3G导航网",
+    "url": "https://3gw.cn/a.php?o=ZZZ",
+    "desc": "3G导航",
+    "avatar": "https://favicone.vercel.app/icon?url=https://3gw.cn/a.php?o=ZZZ",
+    "joinYear": 2026
+  },
+  {
+    "name": "188收录网",
+    "url": "https://www.188dh.cn/",
+    "desc": "188收录网",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.188dh.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "清雨博客",
+    "url": "https://prain.cn/",
+    "desc": "一个极为干净的开源PHP轻博客程序，比一张图片还小，不到150K，不需要数据库，无任何依赖，冰清玉洁，纤尘不染",
+    "avatar": "https://favicone.vercel.app/icon?url=https://prain.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "一望成欢🍀",
+    "url": "https://wanqingcheng.cn",
+    "desc": "一望成欢🍀记录烟火，珍藏热爱",
+    "avatar": "https://favicone.vercel.app/icon?url=https://wanqingcheng.cn",
+    "joinYear": 2026
+  },
+  {
+    "name": "黄鼠狼的树洞",
+    "url": "https://blog.weasel6.cn/",
+    "desc": "小黄的博客，记录生活中的美好",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blog.weasel6.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "雨眠碎碎念",
+    "url": "https://blog.yuumii.top",
+    "desc": "愿君安康~",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blog.yuumii.top",
+    "joinYear": 2026
+  },
+  {
+    "name": "Mystic Stars",
+    "url": "https://www.mysticstars.cn/",
+    "desc": "Discover Infinity",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.mysticstars.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "中文独立博客聚合列表",
+    "url": "https://blogroll.online/",
+    "desc": "又一个博客聚合站",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blogroll.online/",
+    "joinYear": 2026
+  },
+  {
+    "name": "友情网址导航",
+    "url": "http://www.heatsystem.cn/",
+    "desc": "友情网址导航",
+    "avatar": "https://favicone.vercel.app/icon?url=http://www.heatsystem.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "自助链大全",
+    "url": "http://www.zizhulian.cc",
+    "desc": "自助链大全提供自助链接导航网站的收录、展示、推广、广告等。",
+    "avatar": "https://favicone.vercel.app/icon?url=http://www.zizhulian.cc",
+    "joinYear": 2026
+  },
+  {
+    "name": "小景",
+    "url": "https://jingyuan-zheng.github.io/zh/",
+    "desc": "随心分享有趣的技术、开源项目与生活记录。",
+    "avatar": "https://favicone.vercel.app/icon?url=https://jingyuan-zheng.github.io/zh/",
+    "joinYear": 2026
+  },
+  {
+    "name": "88导航网-网站收录-网址收录-网址导航-收录网站-自助广告系统",
+    "url": "https://www.88dhw.top",
+    "desc": "88导航是最新的网址导航程序，自动秒收录，自动审核，收录国内外、各行业优秀网站，旨在为大家提供网站分类目录源码、优秀网站参考、网站推广服务！",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.88dhw.top",
+    "joinYear": 2026
+  },
+  {
+    "name": "Ethan的避风港",
+    "url": "https://www.ishana.cn/",
+    "desc": "野生笨蛋小猫的个人主页",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.ishana.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "壹拾肆",
+    "url": "https://www.kaaaaai.cn/",
+    "desc": "有些安排是多余的 这片空旷对我有益",
+    "avatar": "https://favicone.vercel.app/icon?url=https://www.kaaaaai.cn/",
+    "joinYear": 2026
+  },
+  {
+    "name": "灵狐笑话",
+    "url": "shturl.cc/udiSARM",
+    "desc": "小笑话分享",
+    "avatar": "shturl.cc/FWgwQ4onkd6b5kdwwzKgKPeV0GNDky75qfQPOV9iL9zP",
+    "joinYear": 2026
+  },
+  {
+    "name": "懋和道人",
+    "url": "https://blog.dao.js.cn/",
+    "desc": "李懋和，俗名李栋梁。书法、国画爱好者，互联网安全与前端建设者。",
+    "avatar": "https://favicone.vercel.app/icon?url=https://blog.dao.js.cn/",
+    "joinYear": 2026
+  }
 ];
-
-// 你的接收申请邮箱
-const ADMIN_EMAIL = "yourmail@example.com";
